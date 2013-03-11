6839137385a6f6b61ea0ae9763a76732f335bd7b
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2013-03-11 16:53:39 +0100

    test file for testing database queries

---
+++ src/core/Test.java
@@ -0,0 +1,37 @@
+package core;
+
+import java.util.Properties;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+import db.Factory;
+
+public class Test {
+	
+	public static void main(String args[]) {
+		
+		Properties p = new Properties();
+		
+		try {
+			System.out.println("initializing battlestations");
+			p.load(new FileInputStream(new File("resources/database.properties")));
+			System.out.println(p.toString());
+			
+			Factory f = new Factory(p);
+			System.out.println(f.createUser("aleksander", "passord", "email"));
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
