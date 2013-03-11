5024b872accac058a58a61ebec37199e7e7dff06
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2013-03-11 14:07:29 +0100

    added basic createUser command. this class depends on the not yet implemented DBConnection class

--- src/db/Factory.java
+++ src/db/Factory.java
@@ -1,9 +1,26 @@
 package db;
 
+import java.sql.ResultSet;
+
 public class Factory {
 	
-	public Factory() {
-		// Not yet implemented	
+	DBConnection db;
+	
+	public Factory(Properties properties) {
+		db = new DBConnection(properties);	
+	}
+	
+	public User createUser(String name, String password, String email) {
+		
+		String query = String.format("INSERT INTO user " + 
+		"(name, password, email) VALUES ('%s', '%s', '%s')", name, password, email);
+		
+		db.initialize();
+		db.makeSingleUpdate(query);
+		db.close();
+		
+		User u = new User(name, password, email);
+		return u;
 	}
 	
 }
