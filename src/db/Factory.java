512aff363c2e03d115e2b61e48fe7143633de4bb
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2013-03-11 16:47:11 +0100

    db factory now has fully functioning create user method

--- src/db/Factory.java
+++ src/db/Factory.java
@@ -1,26 +1,48 @@
 package db;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+import java.sql.PreparedStatement;
 
 public class Factory {
 	
 	DBConnection db;
 	
-	public Factory(Properties properties) {
+	public Factory(Properties properties) throws ClassNotFoundException, SQLException {
 		db = new DBConnection(properties);	
 	}
 	
-	public User createUser(String name, String password, String email) {
-		
-		String query = String.format("INSERT INTO user " + 
-		"(name, password, email) VALUES ('%s', '%s', '%s')", name, password, email);
+	public User createUser(String name, String password, String email) throws ClassNotFoundException, SQLException {
+
+		PreparedStatement prest;
+		ResultSet generatedKeys;
+		int userId = 0;
 		
 		db.initialize();
-		db.makeSingleUpdate(query);
+		
+		prest = db.preparedStatement("INSERT INTO sids.user (name, hashedPassword, email) VALUES (?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+		prest.setString(1, name);
+		prest.setString(2, password);
+		prest.setString(3, email);
+		prest.executeUpdate();
+		
+		generatedKeys = prest.getGeneratedKeys();
+		if (generatedKeys.next()) {
+			userId = generatedKeys.getInt(1);
+		}
+		
 		db.close();
 		
-		User u = new User(name, password, email);
-		return u;
+		System.out.println("finished!");
+		
+		if (userId != 0) {
+			return new User((int)userId, name, password, email);
+		} else {
+			return null;
+		}
+		
 	}
 	
 }
