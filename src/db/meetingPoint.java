fdfe73e20ddf4867e6c1251e809e14faed8c5d39
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2013-03-11 12:25:37 +0100

    renamed meetingpoint to uppercase

--- src/db/meetingPoint.java
+++ src/db/meetingPoint.java
@@ -1,11 +1,11 @@
 package db;
 
-public class meetingPoint {
+public class MeetingPoint {
 	private int id;
 	private String name;
 	private int capacity;
 
-	public meetingPoint(int id, String name, int capacity) {
+	public MeetingPoint(int id, String name, int capacity) {
 		this.id = id;
 		this.name = name;
 		this.capacity = capacity;
