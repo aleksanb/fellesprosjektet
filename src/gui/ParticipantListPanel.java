7b8d922c4a9204e630e795d7e5c8f3b655405374
Kathrine Steffensen <kathrine.steffensen@gmail.com>
2013-03-18 11:42:09 +0100

    New methods and an attempt at formatting objects. Heh.

--- src/gui/ParticipantListPanel.java
+++ src/gui/ParticipantListPanel.java
@@ -1,5 +1,6 @@
 package gui;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
@@ -14,11 +15,14 @@ import db.User;
 
 public class ParticipantListPanel {  
    public static void main(String args[]) {
-      JFrame frame = new JFrame();
+	  Dimension d = new Dimension(20,10);
+      JFrame frame = new JFrame("Participants");
+      frame.setSize(d);
+      frame.setResizable(false);
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       User user1 = new User();
       user1.setName("Kathrine Steffensen");
-      JList list = new JList(new CheckListItem[] {new CheckListItem(user1)});
+      JList list = new JList(new CheckListItem[] {new CheckListItem(user1),});
       list.setCellRenderer(new CheckListRenderer());
       list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
       list.addMouseListener(new MouseAdapter() {
@@ -46,38 +50,39 @@ public class ParticipantListPanel {
       frame.setVisible(true);
    }
 
-	public ParticipantList getParticipantList() {
+	public ParticipantListPanel getParticipantList() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+}
+class CheckListItem {
+	protected User user;
+	protected boolean isSelected = false;
 	
-	class CheckListItem {
-	   protected User user;
-	   protected boolean isSelected = false;
-	
-	   public CheckListItem(User u) {
-	      this.user = user;
-	   }
-	
-	   public boolean isSelected() {
-	      return isSelected;
-	   }
-	
-	   public void setSelected(boolean isSelected) {
-	      this.isSelected = isSelected;
-	   }   
+	public CheckListItem(User u) {
+		this.user = user;
 	}
-	 // Handles rendering cells in the list using a check box
 	
-	class CheckListRenderer extends JCheckBox implements ListCellRenderer {
-	   public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean hasFocus) {
-	      setEnabled(list.isEnabled());
-	      setSelected(((CheckListItem)value).isSelected());
-	      setFont(list.getFont());
-	      setBackground(list.getBackground());
-	      setForeground(list.getForeground());
-	      //setText(((CheckListItem)value).getName());		//Need to make CheckListItem a user, or at least make it able to use methods of db.User. Herp
-	      return this;
-	   }
+	public boolean isSelected() {
+	    return isSelected;
+	}
+	
+	public void setSelected(boolean isSelected) {
+		this.isSelected = isSelected;
+	}
+	public String toString() {
+		return user.getName();
+	}
+}
+
+class CheckListRenderer extends JCheckBox implements ListCellRenderer {
+	public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean hasFocus) {
+		setEnabled(list.isEnabled());
+		setSelected(((CheckListItem)value).isSelected());
+		setFont(list.getFont());
+		setBackground(list.getBackground());
+		setForeground(list.getForeground());
+		setText(((CheckListItem)value).toString());
+		return this;
 	}
 }
