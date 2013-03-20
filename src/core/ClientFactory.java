6e8dfdad5ca51920379c943bb38d3d6b571c9a1a
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2013-03-20 02:17:15 +0100

    added appointment method

--- src/core/ClientFactory.java
+++ src/core/ClientFactory.java
@@ -11,7 +11,7 @@ import java.net.Socket;
 import java.util.Properties;
 
 import db.Action;
-import db.JsonFactory;
+import db.Appointment;
 import db.User;
 
 public class ClientFactory {
@@ -19,12 +19,10 @@ public class ClientFactory {
 	private ObjectOutputStream output;
 	private ObjectInputStream input;
 	private Socket connection;
-	private JsonFactory jf;
 	Properties prop;
 	
 	public ClientFactory() {
 		connectToServer();
-		jf = new JsonFactory();
 	}
 	
 	private void connectToServer() {
@@ -62,12 +60,38 @@ public class ClientFactory {
 		logConsole("connection established");
 	}
 	
+	// :D :D :D :D :D :D
+	// Here come the class methods
+	// :D :D :D :D :D :D 
+	
+	public Appointment addAppointment(Appointment appointment) {
+		
+		System.out.println("sending appointmnent to server");
+		//String json = jf.generateJsonCommand(Action.INSERT, appointment);
+		Appointment callback = null;
+		try {
+			output.writeObject(appointment);
+			System.out.println("wrote object");
+			output.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("waiting for response");
+		try {
+			callback = (Appointment) input.readObject();
+			System.out.println("we have read callback");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return callback;
+	}
+	
 	public void logout(User u){
 		//TODO: use this method when log out button is pushed
-		System.out.println("Going down with the ship, captain!");
-		String json = jf.generateJsonCommand(Action.LOGOUT, u);
 		try {
-			output.writeObject(json);
+			output.writeObject(u);
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
@@ -82,36 +106,31 @@ public class ClientFactory {
 	}
 	
 	public User login(User u) {
-		String json = jf.generateJsonCommand(Action.LOGIN, u);
-		//String callback = "false";
-		User login = null;
+		User callback = null;
 		try {
-			output.writeObject(json);
+			output.writeObject(u);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		System.out.println("waiting for response");
 		try {
-			String temp = (String) input.readObject();
-			if (temp.equals("null")) {
-				return null;
-			} else {
-				login = jf.generateUser(temp);				
-			}
+			callback = (User) input.readObject();	
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
-		return login;
+		return callback;
 	}
 	
 	public static void main(String args[]) {
 		ClientFactory fc = new ClientFactory();
-		User login = fc.login(new User("aleksander", "yolo", "passord"));
+		User u = new User(0, "aleksander", "yolo", "passord");
+		u.setAction(Action.LOGIN);
+		User login = fc.login(u);
 		System.out.println("login returned with " + ((login != null)? login.getName() : "null") );
-		fc.logout(new User("aleksander", "stuff", "passord"));
+		fc.logout(u);
 	}
 	
 	private void logConsole(String text){
