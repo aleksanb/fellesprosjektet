be9ce8ed5788f84c0e36716d2371c2a838a71b38
Petter Astrup <petter.astrup@gmail.com>
2013-03-20 13:33:54 +0100

    Made the panel.Needs inprovment... Espen? :)

--- src/gui/EditAppointmentPanel.java
+++ src/gui/EditAppointmentPanel.java
@@ -5,14 +5,15 @@ import javax.swing.JPanel;
 import core.CalendarProgram;
 import db.Appointment;
 
-public class EditAppointmentPanel extends JPanel{
-	//TODO make the panel
-	public EditAppointmentPanel(CalendarProgram calendarProgram) {
+public class EditAppointmentPanel extends AddAppointmentPanel{
+	//TODO make the panel work properly... Esp?
+	public EditAppointmentPanel(CalendarProgram calendarProgram, Appointment appointment) {
+		super(calendarProgram, appointment);
 		System.out.println("holla mamma");
 	}
 
 	public void setAppoitment(Appointment appointment) {
-		// TODO Auto-generated method stub
+		System.out.println(appointment.getTitle());
 		
 	}
 
