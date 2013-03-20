9acb6ceb7b4ef2731dd235ff22e471d7d2708809
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2013-03-20 02:19:21 +0100

    added appointment method

--- src/server/ServerFactory.java
+++ src/server/ServerFactory.java
@@ -7,9 +7,14 @@ import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import java.util.Properties;
 import java.sql.PreparedStatement;
 
+import db.Appointment;
+import db.MeetingPoint;
 import db.User;
 
 
@@ -39,13 +44,20 @@ public class ServerFactory {
 	public static void main(String args[]) {
 		ServerFactory sf = new ServerFactory();
 		System.out.println("created serverFactory");
-		String result = sf.login(new User("aleksander", "email", "passord"));
+		//User u = new User(1, "espen", "master@commander.net", "hunter2");
+		Appointment a = new Appointment(0, 1, "title", new GregorianCalendar(), new GregorianCalendar(), "first test meeting", true);
+		a.setMeetingPoint(new MeetingPoint(1, "mordi", 200));
+		a.addParticipant(new User(1, "espen", "master@commander.net", "hunter2"));
+		a.addParticipant(new User(8, "aleksander", "email", "passord"));
+		//System.out.println(a.getMeetingPoint().getId());
+		//User result = sf.login(new User("aleksander", "email", "passord"));
+		Appointment result = sf.insertAppointment(a);
 		System.out.println(result);
 	}
 	
-	public String login(User u) {
+	public User login(User u) {
 		PreparedStatement prest;
-		String rslt = "null";
+		User result = null;
 		try {
 			System.out.println("preparing to check user");
 			//System.out.println(p);
@@ -59,8 +71,7 @@ public class ServerFactory {
 			ResultSet rs = prest.executeQuery();
 			
 			while (rs.next()) {
-				rslt = String.format("{'id':'%d','name':'%s','hashedPassword':'%s','email':'%s'}", 
-						rs.getInt("id"), rs.getString("name"), rs.getString("email"), rs.getString("hashedPassword"));
+				result = new User(rs.getInt("id"), rs.getString("name"), rs.getString("email"), rs.getString("hashedPassword"));
 			}
 				
 			db.close();
@@ -69,7 +80,7 @@ public class ServerFactory {
 			e.printStackTrace();
 			System.out.println("something fucked up");
 		}
-		return rslt;
+		return result;
 	}
 	
 	public User createUser(String name, String password, String email) throws ClassNotFoundException, SQLException {
@@ -101,6 +112,81 @@ public class ServerFactory {
 			return null;
 		}
 		
+	}
+	
+	public Appointment insertAppointment(Appointment appointment) {
+		PreparedStatement prest;
+		ResultSet generatedKeys;
+		int appointmentId = 0;
+		try {
+			db.initialize();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			// create and insert new appointment into database
+			prest = db.preparedStatement("INSERT INTO sids.appointment (creatorUserId, title, start, end, description, isMeeting)"+
+			" VALUES (?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+			prest.setInt(1, appointment.getId());
+			prest.setString(2, appointment.getTitle());
+			prest.setTimestamp(3, new Timestamp(appointment.getStart().getTimeInMillis()));
+			prest.setTimestamp(4, new Timestamp(appointment.getEnd().getTimeInMillis()));
+			prest.setString(5, appointment.getDescription());
+			prest.setBoolean(6, appointment.isMeeting());
+			System.out.println("executing appointment insert");
+			prest.executeUpdate();
+			
+			generatedKeys = prest.getGeneratedKeys();
+			if (generatedKeys.next()) { // if successfully inserted
+				appointmentId = generatedKeys.getInt(1);
+				
+				// add appointment to creator
+				prest = db.preparedStatement("INSERT INTO sids.user_appointment (userId, appointmentId) VALUES (?, ?)");
+				prest.setInt(1, appointment.getCreatorUserId());
+				prest.setInt(2, appointmentId);
+				prest.executeUpdate();
+				
+				if (appointment.isMeeting()) { 
+					prest = db.preparedStatement("INSERT INTO sids.user_appointment (userId, appointmentId) VALUES (?, ?)");
+					ArrayList<User> participants = appointment.getParticipants();
+					
+					// create user-appointment connection for all users
+					for (int i = 0; i < participants.size(); i++) { 
+						System.out.println("executing userAppointment nr. " + i + " " + participants.get(i).getName());
+						prest.setInt(1, participants.get(i).getId());
+						prest.setInt(2, appointmentId);
+						prest.executeUpdate();
+					}
+					// create appointment - room connection
+					System.out.println("executing appointmentMeetingpoint");
+					prest = db.preparedStatement("INSERT INTO sids.appointment_meetingpoint (appointmentId, meetingpointId) VALUES(?, ?)");
+					prest.setInt(1, appointmentId);
+					prest.setInt(2, appointment.getMeetingPoint().getId());
+					System.out.println("executing appointmentMeetingPoint");
+					prest.executeUpdate();
+					
+				}
+			}
+			
+			System.out.println("finished inserting");
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			db.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		if (appointmentId != 0) {
+			appointment.setId(appointmentId);
+			return appointment;
+		} else {
+			return null;
+		}
 	}
 	
 }
