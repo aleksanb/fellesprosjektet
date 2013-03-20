4576189adde9e5a5fd46a28d96d41baa364069c6
Petter Astrup <petter.astrup@gmail.com>
2013-03-20 02:47:20 +0100

    created panel for editing appointments

---
+++ src/gui/EditAppointmentPanel.java
@@ -0,0 +1,19 @@
+package gui;
+
+import javax.swing.JPanel;
+
+import core.CalendarProgram;
+import db.Appointment;
+
+public class EditAppointmentPanel extends JPanel{
+	//TODO make the panel
+	public EditAppointmentPanel(CalendarProgram calendarProgram) {
+		System.out.println("holla mamma");
+	}
+
+	public void setAppoitment(Appointment appointment) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
