6ea1793d2ed59e2ecf493a60ef2380db6bb141eb
Espen Hellerud <espenhellerud5@gmail.com>
2013-03-21 14:56:47 +0100

    updated models to fit with sending msg over internetts

--- src/db/Notification.java
+++ src/db/Notification.java
@@ -2,18 +2,25 @@ package db;
 
 import java.io.Serializable;
 
-public class Notification implements Serializable {
+public class Notification implements Serializable, AbstractModel{
 	private static final long serialVersionUID = 1L;
 	private int userId;
 	private Appointment appointment;
 	private NotificationType notificationType;
 	private String message;
+	private Action action;
 	
 
 	public Notification(int userId, Appointment appointment, NotificationType notificationType){
 		this.userId = userId;
 		this.appointment = appointment;
 		this.notificationType = notificationType;	
+	}
+	public Notification(int userId, Appointment appointment, NotificationType notificationType,String message){
+		this.userId = userId;
+		this.appointment = appointment;
+		this.notificationType = notificationType;
+		this.message=message;
 	}
 	
 	public String getMessage() {
@@ -50,6 +57,22 @@ public class Notification implements Serializable {
 	@Override
 	public String toString(){
 		return message;
+	}
+
+	@Override
+	public void setAction(Action action) {
+		this.action=action;
+		
+	}
+
+	@Override
+	public Action getAction() {
+		return action;
+	}
+
+	@Override
+	public <T> T getCopy() {
+		return (T) new Notification(this.userId, (Appointment) this.appointment.getCopy(), this.notificationType,this.message);
 	}
 	
 }
